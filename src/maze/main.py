1c5c80c96c8ea8b15d1799f25b3920ecba3010a4
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2025-07-11 23:00:28 +0200

    Deleted pycaches

--- src/maze/main.py
+++ src/maze/main.py
@@ -1,10 +1,7 @@
 import pygame
 import sys
 
-from pygame.locals import * # type: ignore
-
-from core import Maze
-from views import MazeView
+from pygame.locals import * #type: ignore
 
 pygame.init()
 
@@ -17,15 +14,11 @@ if surface == None: raise ValueError("No surface found")
 
 fps = pygame.time.Clock()
 
-mazeview = MazeView(surface, Maze((0, 0)))
-
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
-    mazeview.draw()
 
     pygame.display.update()
     fps.tick(60)
--- src/maze/views/maze_view.py
+++ src/maze/views/maze_view.py
@@ -6,6 +6,16 @@ from . import View
 from locals import *
 
 class MazeView(View):
-    def __init__(self, maze: Maze, size: tuple[int, int], center: tuple[int, int]) -> None:
+    def __init__(self, 
+                 maze: Maze, 
+                 size: tuple[int, int], 
+                 center: tuple[int, int]) -> None:
         super().__init__(size, center)
         self._maze = maze
+
+    def draw(self, 
+             target: Surface, 
+             center: tuple[int, int] | None = None, 
+             size: tuple[int, int] | None = None):
+        super().draw(target, center, size)
+        self._surface.fill(BLUE)
